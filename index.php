e19597a74a63ba435f9e6dc4e5ec8b9af93a9c02
muhammmad yusuf <muh.joseph27@gmail.com>
2021-07-17 16:03:45 +0700

    change minor file name

--- index.php
+++ index.php
@@ -1,315 +1,198 @@
-<?php session_start();
-error_reporting(E_ERROR | E_PARSE);
+<?php  session_start();
 
-if(isset($_SESSION['username'])){
+include "conn.php";
+// error_reporting(E_ERROR | E_PARSE);
 
+if (isset($_POST['login'])){
 	//koneksi terpusat
-	include "conn.php";
-	$username=$_SESSION['username'];
+
+	$username=$_POST['username'];
+	$password=md5($_POST['password']);
+	$domain=$_POST['domain'];
+	
+	if($domain === "admin"){
+		$query=mysqli_query($koneksi,"select * from user_admin where username='$username' and password='$password'");
+		$cek=mysqli_num_rows($query);
+		$row=mysqli_fetch_array($query);
+		$id_admin=$row['id_admin'];
+		
+		if($cek){
+			$_SESSION['username']=$username;
+			$_SESSION['id_admin']=$id_admin;
+			$_SESSION['domain']=$domain;
+			$_SESSION['waktu']=date("Y-m-d H:i:s");
+			
+			?><script language="javascript">
+	document.location.href = "home.php";
+</script><?php
+			
+		}else{
+			?><script language="javascript">
+	document.location.href = "index.php?status=Gagal Login";
+</script><?php
+		}	
+	}
+	
+	if($domain === "guru"){
+		$query=mysqli_query($koneksi,"select * from data_guru where username='$username' and password='$password'");
+		$cek=mysqli_num_rows($query);
+		$row=mysqli_fetch_array($query);
+		$id_siswa=$row['nip'];
+		
+		if($cek){
+			$_SESSION['username']=$username;
+			$_SESSION['nip']=$nip;
+			$_SESSION['waktu']=date("Y-m-d H:i:s");
+			$_SESSION['domain']=$domain;
+			
+			?><script language="javascript">
+	document.location.href = "home.php";
+</script><?php
+			
+		}else{
+			?><script language="javascript">
+	document.location.href = "index.php?status=Gagal Login";
+</script><?php
+		}
+	}
+	
+	if($domain === "siswa"){
+		$query=mysqli_query($koneksi,"select * from data_siswa where username='$username' and password='$password'");
+		$cek=mysqli_num_rows($query);
+		$row=mysqli_fetch_array($query);
+		$id_siswa=$row['nis'];
+		
+		if($cek){
+			$_SESSION['username']=$username;
+			$_SESSION['id_siswa']=$id_siswa;
+			$_SESSION['waktu']=date("Y-m-d H:i:s");
+			$_SESSION['domain']=$domain;
+			
+			?><script language="javascript">
+	document.location.href = "home.php";
+</script><?php
+			
+		}else{
+			?><script language="javascript">
+	document.location.href = "index.php?status=Gagal Login";
+</script><?php
+		}
+	}
+
+	if($domain === "ortu"){
+		$query=mysqli_query($koneksi,"select * from tbl_ortu where username='$username' and password='$password'");
+		$cek=mysqli_num_rows($query);
+		$row=mysqli_fetch_array($query);
+		$id_siswa=$row['id'];
+		
+		if($cek){
+			$_SESSION['username']=$username;
+			$_SESSION['id']=$id;
+			$_SESSION['waktu']=date("Y-m-d H:i:s");
+			$_SESSION['domain']=$domain;
+			
+			?><script language="javascript">
+	document.location.href = "home.php";
+</script><?php
+			
+		}else{
+			?><script language="javascript">
+	document.location.href = "index.php?status=Gagal Login";
+</script><?php
+		}
+	}
+			
+}else{
+	unset($_POST['login']);
+}
 ?>
 
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <link rel="icon" href="./images/png-clipart-higher-education-student-affairs-education-symbol-university-adult-education.png" type="image/png">
-    <title>S.I.N.O (Sistem Informasi Nilai Online)</title>
-        <link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" title="default" />
-        <!--  jquery core -->
-        <script src="js/jquery/jquery-1.4.1.min.js" type="text/javascript"></script>
-        
-        <!--  checkbox styling script -->
-        <script src="js/jquery/ui.core.js" type="text/javascript"></script>
-        <script src="js/jquery/ui.checkbox.js" type="text/javascript"></script>
-        <script src="js/jquery/jquery.bind.js" type="text/javascript"></script>
-        <script type="text/javascript">
-        $(function(){
-            $('input').checkBox();
-            $('#toggle-all').click(function(){
-            $('#toggle-all').toggleClass('toggle-checked');
-            $('#mainform input[type=checkbox]').checkBox('toggle');
-            return false;
-            });
-        });
-        </script>  
-        
-        <![if !IE 7]>
-        
-        <!--  styled select box script version 1 -->
-        <script src="js/jquery/jquery.selectbox-0.5.js" type="text/javascript"></script>
-        <script type="text/javascript">
-        $(document).ready(function() {
-            $('.styledselect').selectbox({ inputClass: "selectbox_styled" });
-        });
-        </script>
-        
-        <![endif]>
-        
-        <!--  styled select box script version 2 --> 
-        <script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
-        <script type="text/javascript">
-        $(document).ready(function() {
-            $('.styledselect_form_1').selectbox({ inputClass: "styledselect_form_1" });
-            $('.styledselect_form_2').selectbox({ inputClass: "styledselect_form_2" });
-        });
-        </script>
-        
-        <!--  styled select box script version 3 --> 
-        <script src="js/jquery/jquery.selectbox-0.5_style_2.js" type="text/javascript"></script>
-        <script type="text/javascript">
-        $(document).ready(function() {
-            $('.styledselect_pages').selectbox({ inputClass: "styledselect_pages" });
-        });
-        </script>
-        
-        <!--  styled file upload script --> 
-        <script src="js/jquery/jquery.filestyle.js" type="text/javascript"></script>
-        <script type="text/javascript" charset="utf-8">
-          $(function() {
-              $("input.file_1").filestyle({ 
-                  image: "images/forms/choose-file.gif",
-                  imageheight : 21,
-                  imagewidth : 78,
-                  width : 310
-              });
-          });
-        </script>
-        
-        <!-- Custom jquery scripts -->
-        <script src="js/jquery/custom_jquery.js" type="text/javascript"></script>
-         
-        <!-- Tooltips -->
-        <script src="js/jquery/jquery.tooltip.js" type="text/javascript"></script>
-        <script src="js/jquery/jquery.dimensions.js" type="text/javascript"></script>
-        <script type="text/javascript">
-        $(function() {
-            $('a.info-tooltip ').tooltip({
-                track: true,
-                delay: 0,
-                fixPNG: true, 
-                showURL: false,
-                showBody: " - ",
-                top: -35,
-                left: 5
-            });
-        });
-        </script> 
-        
-        
-        <!--  date picker script -->
-        <link rel="stylesheet" href="css/datePicker.css" type="text/css" />
-        <script src="js/jquery/date.js" type="text/javascript"></script>
-        <script src="js/jquery/jquery.datePicker.js" type="text/javascript"></script>
-        <script type="text/javascript" charset="utf-8">
-                $(function()
-        {
-        
-        // initialise the "Select date" link
-        $('#date-pick')
-            .datePicker(
-                // associate the link with a date picker
-                {
-                    createButton:false,
-                    startDate:'01/01/2005',
-                    endDate:'31/12/2020'
-                }
-            ).bind(
-                // when the link is clicked display the date picker
-                'click',
-                function()
-                {
-                    updateSelects($(this).dpGetSelected()[0]);
-                    $(this).dpDisplay();
-                    return false;
-                }
-            ).bind(
-                // when a date is selected update the SELECTs
-                'dateSelected',
-                function(e, selectedDate, $td, state)
-                {
-                    updateSelects(selectedDate);
-                }
-            ).bind(
-                'dpClosed',
-                function(e, selected)
-                {
-                    updateSelects(selected[0]);
-                }
-            );
-            
-        var updateSelects = function (selectedDate)
-        {
-            var selectedDate = new Date(selectedDate);
-            $('#d option[value=' + selectedDate.getDate() + ']').attr('selected', 'selected');
-            $('#m option[value=' + (selectedDate.getMonth()+1) + ']').attr('selected', 'selected');
-            $('#y option[value=' + (selectedDate.getFullYear()) + ']').attr('selected', 'selected');
-        }
-        // listen for when the selects are changed and update the picker
-        $('#d, #m, #y')
-            .bind(
-                'change',
-                function()
-                {
-                    var d = new Date(
-                                $('#y').val(),
-                                $('#m').val()-1,
-                                $('#d').val()
-                            );
-                    $('#date-pick').dpSetSelected(d.asString());
-                }
-            );
-        
-        // default the position of the selects to today
-        var today = new Date();
-        updateSelects(today.getTime());
-        
-        // and update the datePicker to reflect it...
-        $('#d').trigger('change');
-        });
-        </script>
-        
-        <!-- MUST BE THE LAST SCRIPT IN <HEAD></HEAD></HEAD> png fix -->
-        <script src="js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>
-        <script type="text/javascript">
-        $(document).ready(function(){
-        $(document).pngFix( );
-        });
-        </script>
-    </head>
-    
-    <body> 
-    <!-- Start: page-top-outer -->
-    <div id="page-top-outer">    
-    
-    <!-- Start: page-top -->
-    <div id="page-top">
-    
-        <!-- start logo -->
-        <div id="logo">
-        <a href=""><img src="images/shared/logo.png" width="156" height="40" alt="" /></a>
-        </div>
-        <!-- end logo -->
-        
-        <!--  start top-search -->
-        <div id="top-search">
-        </div>
-        <!--  end top-search -->
-        <div class="clear"></div>
-    
-    </div>
-    <!-- End: page-top -->
-    
-    </div>
-    <!-- End: page-top-outer -->
-        
-    <div class="clear">&nbsp;</div>
-     
-    <!--  start nav-outer-repeat................................................................................................. START -->
-    <div class="nav-outer-repeat"> 
-    <!--  start nav-outer -->
-    <div class="nav-outer"> 
-    
-            <!-- start nav-right -->
-            <div id="nav-right">
-            
-                <div class="nav-divider">&nbsp;</div>
-                <div class="showhide-account"><img src="images/shared/nav/nav_myaccount.gif" width="93" height="14" alt="" /></div>
-                <div class="nav-divider">&nbsp;</div>
-                <a href="logout.php" id="logout" onclick="return confirm('Apakah Anda yakin?')"><img src="images/shared/nav/nav_logout.gif" width="64" height="14" alt="" /></a>
-                <div class="clear">&nbsp;</div>
-            
-                <!--  start account-content -->	
-                <div class="account-content">
-                <div class="account-drop-inner">
-                    <a href="" id="acc-settings">Settings</a>
-                    <div class="clear">&nbsp;</div>
-                    <div class="acc-line">&nbsp;</div>
-                    <a href="" id="acc-inbox">Inbox</a>
-                    <div class="clear">&nbsp;</div>
-                </div>
-                </div>
-                <!--  end account-content -->
-            
-            </div>
-            <!-- end nav-right -->
-    
-    
-            <!--  start nav -->
-            <div class="nav">
-            <div class="table">
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-    		<?php 
-			$domain=$_SESSION['domain'];
-			
-			if($domain=='admin'){
-				include "menu-admin.php";
-			}
-			
-			if($domain=='guru'){
-				include "menu-guru.php";
-			}
-			
-			if($domain=='siswa'){
-				include "menu-siswa.php";
-			}
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>S.I.N.O (Sistem Informasi Nilai Online)</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="css/screen.css" type="text/css" media="screen" title="default" />
+	<!--  jquery core -->
+	<script src="js/jquery/jquery-1.4.1.min.js" type="text/javascript"></script>
 
-            if($domain=='ortu'){
-				include "menu-ortu.php";
-			}
-			
-			?>
+	<!-- Custom jquery scripts -->
+	<script src="js/jquery/custom_jquery.js" type="text/javascript"></script>
 
-            <div class="nav-divider">&nbsp;</div>
-    
-    
-            <div class="clear"></div>
-            </div>
-            <div class="clear"></div>
-            </div>
-            <!--  start nav -->
-    
-    </div>
-    <div class="clear"></div>
-    <!--  start nav-outer -->
-    </div>
-    <!--  start nav-outer-repeat................................................... END -->
-    
-      <div class="clear"></div>
-     
-    <!-- start content-outer ........................................................................................................................START -->
-    <div id="content-outer">
-    <!-- start content -->
-    <div id="content">
-    
-    	<?php include "content.php"; ?>
-        
-        <div class="clear">&nbsp;</div>
-    
-    </div>
-    <!--  end content -->
-    <div class="clear">&nbsp;</div>
-    </div>
-    <!--  end content-outer........................................................END -->
-    
-    <div class="clear">&nbsp;</div>
-        
-    <!-- start footer -->         
-    <div id="footer">
-    <!-- <div id="footer-pad">&nbsp;</div> -->
-        <!--  start footer-left -->
-        <div id="footer-left">
-        Admin Skin by <span id="spanYear"></span> <a href="http://www.netdreams.co.uk" target="_blank">www.netdreams.co.uk</a> S.I.N.O &copy; Copyright <a href="http://ri32.wordpress.com" target="_blank">Ri32 Network Project.</a> All rights reserved.</div>
-        <!--  end footer-left -->
-        <div class="clear">&nbsp;</div>
-    </div>
-    <!-- end footer -->
-     
-    </body>
-    </html>
-    
-<?php
-}else{
-	session_destroy();
-	header('Location:index.php?status=Silahkan Login');
-}
-?>	
+	<!-- MUST BE THE LAST SCRIPT IN <HEAD></HEAD></HEAD> png fix -->
+	<script src="js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$(document).ready(function () {
+			$(document).pngFix();
+		});
+	</script>
+</head>
+
+<body id="login-bg" onLoad="document.postform.elements['username'].focus();">
+
+	<!-- Start: login-holder -->
+	<div id="login-holder">
+
+		<!-- start logo -->
+		<div id="logo-login">
+			<a href="index.html"><img src="images/shared/logo.png" width="156" height="40" alt="" /></a>
+		</div>
+		<!-- end logo -->
+
+		<div class="clear"></div>
+
+		<!--  start loginbox ................................................................................. -->
+		<div id="loginbox">
+
+			<!--  start login-inner -->
+			<div id="login-inner">
+				<p align="center">
+					<font face="verdana" size="2" color="#333333">
+						<?php  if(isset($_GET['status'])){ echo "&laquo;".$_GET['status']."&raquo;"; }?></font>
+				</p>
+				<p>&nbsp;</p>
+				<form action="index.php" method="post" name="postform">
+					<table border="0" cellpadding="0" cellspacing="0">
+						<tr>
+							<th>Username</th>
+							<td><input type="text" class="login-inp" name="username" /></td>
+						</tr>
+						<tr>
+							<th>Password</th>
+							<td><input type="password" value="************" name="password" onFocus="this.value=''"
+									class="login-inp" /></td>
+						</tr>
+						<tr>
+							<th>Akses</th>
+							<td>
+								<select name="domain" class="styledselect">
+									<option value="admin"> ADMIN </option>
+									<option value="guru"> GURU </option>
+									<option value="siswa"> SISWA </option>
+									<option value="ortu"> ORANG TUA </option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<th></th>
+							<td><input type="submit" class="submit-login" name="login" /></td>
+						</tr>
+					</table>
+				</form>
+			</div>
+
+
+
+			<!--  end login-inner -->
+			<div class="clear"></div>
+		</div>
+		<!--  end loginbox -->
+
+	</div>
+	<!-- End: login-holder -->
+</body>
+
+</html>
